3f8d1e3452f8bd0c088acf9267e4850739051fc3
Peter Sherif <petershreif@gmail.com>
2018-02-15 00:06:08 +0200

    Replaced CDN libs with local libs: Bootstrap, jquery, etc.

--- dashboard/index.php
+++ dashboard/index.php
@@ -6,22 +6,23 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name="description" content="">
-
+	
+	<!-- favicon -->
 	<link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
 	<link rel="icon" href="/img/favicon.ico" type="image/x-icon">
-	<title>BVN - Home</title>
+	<title>BVN - Dashboard</title>
 
 	<!-- Normalize libirary - To Make browsers render all elements more consistently. -->
-	<link href="https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.min.css" rel="stylesheet">
+	<link href="/vendor/normalize.min.css" rel="stylesheet">
 
 	<!-- Bootstrap v.3.3.7 CSS -->
-	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<link href="/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
 	<!-- Tachyons css library - Allows us to write less custom css code -->
-	<link rel="stylesheet" href="https://unpkg.com/tachyons/css/tachyons.min.css">
+	<link rel="stylesheet" href="vendor/tachyons/css/tachyons.min.css">
 
 	<!-- Fontawesome library - for icons -->
-	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+	<link href="/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet">
 
 	<!-- Custom Fonts -->
 	<link href='https://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
@@ -92,12 +93,12 @@
 
 	<!-- *********** Javascript *********** -->
 
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+	<!-- jQuery v.3.3.1 Library -->
+	<script src="/vendor/jquery/jquery.min.js"></script>
 
 	<!-- Bootstrap v.3.3.7 JS -->
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+	<script src="/vendor/bootstrap/js/bootstrap.min.js"></script>
 
-	<!-- jQuery v.3.3.1 Library -->
 	<!-- Custom JS -->
 	<script src="/js/main.js"></script>
 </body>
