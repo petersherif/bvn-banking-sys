2354945eeccdb1ce77ddb2d6abcc2f8a96df863b
Peter Sherif <petershreif@gmail.com>
2018-02-26 11:47:15 +0200

    Create Add Employee Page

--- add-employee.php
+++ add-employee.php
@@ -52,20 +52,20 @@
 	<!-- Sidebar -->
 	<?php
 
-		$newClientActive = 'active';
+		$AddEmpActive = 'active';
 		include_once($templates . 'sidebar.php');
 
 	?>
 
 	
 
-	<!-- New client form -->
-	<section class="new-client-section">
+	<!-- New Employee form -->
+	<section class="new-employee-section">
 		<div class="container">
 
 			<div class="row">
 				<div class="col-xs-12 text-center">
-					<h2 class="section__heading text-center color-text">Add New Client</h2>
+					<h2 class="section__heading text-center color-text">Add New Employee</h2>
 				</div> <!-- Section Heading -->
 			</div> <!-- Heading Row -->
 
@@ -77,23 +77,23 @@
 						
 							<div class="form-group">
 								<i class="fa fa-user"></i>
-								<input type="text" placeholder="Enter the Client's Full Name" id="fullname" class="form-control">
+								<input type="text" placeholder="Enter the Employee's Full Name" id="fullname" class="form-control">
 							</div>
 
 							<div class="form-group">
 								<i class="fa fa-barcode"></i>
-								<input type="text" placeholder="Enter the Client's Nationallity ID" id="nat-id" class="form-control">
+								<input type="text" placeholder="Enter the Employee's Nationallity ID" id="nat-id" class="form-control">
 							</div>
 
 							<div class="form-group">
 								<i class="fa fa-birthday-cake"></i>
-								<input type="date" placeholder="Enter the Client's Birthdate" id="birthdate" class="form-control">
+								<input type="date" placeholder="Enter the Employee's Birthdate" id="birthdate" class="form-control">
 							</div>
 
 							<div class="form-group">
 								<i class="fa fa-transgender"></i>
 								<select id="gender" class="form-control">
-									<option>Select the Client's Gender</option>
+									<option>Select the Employee's Gender</option>
 									<option>Male</option>
 									<option>Female</option>
 								</select>
@@ -101,36 +101,36 @@
 
 							<div class="form-group">
 								<i class="fa fa-map-marker"></i>
-								<input type="text" placeholder="Enter the Client's Address" id="address" class="form-control">
+								<input type="text" placeholder="Enter the Employee's Address" id="address" class="form-control">
 							</div>
 
 							<div class="form-group">
 								<i class="fa fa-envelope"></i>
-								<input type="text" placeholder="Enter the Client's Email Address" id="email" class="form-control">
+								<input type="text" placeholder="Enter the Employee's Email Address" id="email" class="form-control">
 							</div>
 
 							<div class="form-group">
 								<i class="fa fa-phone"></i>
-								<input type="text" placeholder="Enter the Client's Phone Number" id="phone-num" class="form-control">
+								<input type="text" placeholder="Enter the Employee's Phone Number" id="phone-num" class="form-control">
 							</div>
 
 							<div class="form-group">
 								<i class="fa fa-thumbs-up"></i>
-								<input type="text" placeholder="Scan the Client's Fingerprint" id="fingerprint" class="form-control">
+								<input type="text" placeholder="Scan the Employee's Fingerprint" id="fingerprint" class="form-control">
 							</div>
 
 							<div class="form-group">
 								<i class="fa fa-user-circle"></i>
-								<input type="file" placeholder="Upload the Client's Picture" id="picture" class="form-control">
+								<input type="file" placeholder="Upload the Employee's Picture" id="picture" class="form-control">
 							</div>
 						
 							<div class="form-group">
-								<input type="submit" value="add new client" class="submit form-control btn btn-block btn-primary">
+								<input type="submit" value="add new Employee" class="submit form-control btn btn-block btn-primary">
 							</div>
 						
 						</form>
 					</div>
-				</div> <!-- New client Form -->
+				</div> <!-- New Employee Form -->
 			</div> <!-- Row -->
 		</div> <!-- Container -->
 	</section>
