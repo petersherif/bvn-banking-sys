376a0246934cfce6b725515051f11c588724f68a
Abdo Osama <brosly89@gmail.com>
2018-03-02 19:40:01 +0200

    card number F022 finished

--- bvn_system.sql
+++ bvn_system.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.5.2
+-- version 4.7.4
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Feb 22, 2018 at 06:04 PM
--- Server version: 10.1.21-MariaDB
--- PHP Version: 7.1.1
+-- Host: 127.0.0.1:3306
+-- Generation Time: Feb 28, 2018 at 08:51 AM
+-- Server version: 5.7.19
+-- PHP Version: 7.1.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -26,12 +28,16 @@ SET time_zone = "+00:00";
 -- Table structure for table `accounts`
 --
 
-CREATE TABLE `accounts` (
-  `id` int(255) NOT NULL,
+DROP TABLE IF EXISTS `accounts`;
+CREATE TABLE IF NOT EXISTS `accounts` (
+  `id` int(255) NOT NULL AUTO_INCREMENT,
   `acc_num` int(255) NOT NULL,
   `balance` int(255) NOT NULL,
   `user_id` int(255) NOT NULL,
-  `bank_id` int(255) NOT NULL
+  `bank_id` int(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_acc` (`user_id`),
+  KEY `bank_acc` (`bank_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -40,12 +46,15 @@ CREATE TABLE `accounts` (
 -- Table structure for table `banks`
 --
 
-CREATE TABLE `banks` (
-  `id` int(255) NOT NULL,
+DROP TABLE IF EXISTS `banks`;
+CREATE TABLE IF NOT EXISTS `banks` (
+  `id` int(255) NOT NULL AUTO_INCREMENT,
   `bank_name` varchar(255) CHARACTER SET utf8 NOT NULL,
   `bank_address` varchar(255) CHARACTER SET utf8 NOT NULL,
   `bank_phone` varchar(255) CHARACTER SET utf8 NOT NULL,
-  `user_id` int(255) NOT NULL
+  `user_id` int(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -54,12 +63,17 @@ CREATE TABLE `banks` (
 -- Table structure for table `bvn`
 --
 
-CREATE TABLE `bvn` (
-  `id` int(255) NOT NULL,
+DROP TABLE IF EXISTS `bvn`;
+CREATE TABLE IF NOT EXISTS `bvn` (
+  `id` int(255) NOT NULL AUTO_INCREMENT,
   `bvn_num` int(255) NOT NULL,
   `user_id` int(255) NOT NULL,
   `bank_id` int(255) NOT NULL,
-  `acc_id` int(255) NOT NULL
+  `acc_id` int(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `acc_id` (`acc_id`),
+  KEY `bank_bvn` (`bank_id`),
+  KEY `user_bvn` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -68,11 +82,14 @@ CREATE TABLE `bvn` (
 -- Table structure for table `transaction`
 --
 
-CREATE TABLE `transaction` (
-  `id` int(255) NOT NULL,
+DROP TABLE IF EXISTS `transaction`;
+CREATE TABLE IF NOT EXISTS `transaction` (
+  `id` int(255) NOT NULL AUTO_INCREMENT,
   `amount` int(255) NOT NULL,
   `type` int(255) NOT NULL DEFAULT '0' COMMENT 'type of transaction (0):deposeit , (1) withdraw',
-  `acc_id` int(255) NOT NULL
+  `acc_id` int(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `acc_transaction` (`acc_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -81,11 +98,15 @@ CREATE TABLE `transaction` (
 -- Table structure for table `transfer`
 --
 
-CREATE TABLE `transfer` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `transfer`;
+CREATE TABLE IF NOT EXISTS `transfer` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `amount` int(11) NOT NULL,
   `sender_id` int(11) NOT NULL,
-  `reciever_id` int(11) NOT NULL
+  `reciever_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `acc_transfer` (`sender_id`),
+  KEY `acc_transaction2` (`reciever_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -94,8 +115,9 @@ CREATE TABLE `transfer` (
 -- Table structure for table `users`
 --
 
-CREATE TABLE `users` (
-  `id` int(255) NOT NULL,
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(255) NOT NULL AUTO_INCREMENT,
   `user_name` varchar(255) CHARACTER SET utf8 NOT NULL,
   `email` varchar(255) CHARACTER SET utf8 NOT NULL,
   `password` varchar(255) CHARACTER SET utf8 NOT NULL,
@@ -105,102 +127,14 @@ CREATE TABLE `users` (
   `address` varchar(255) CHARACTER SET utf8 NOT NULL,
   `phone` varchar(255) CHARACTER SET utf8 NOT NULL,
   `thumb` varchar(255) CHARACTER SET utf8 NOT NULL COMMENT 'picture of user',
-  `auth` int(255) NOT NULL DEFAULT '0' COMMENT 'user authorization:- (0)client_auth, (1)Emp_auth (2)Bank_manager_auth, (3)Central_bank_manager'
+  `auth` int(255) NOT NULL DEFAULT '0' COMMENT 'user authorization:- (0)client_auth, (1)Emp_auth (2)Bank_manager_auth, (3)Central_bank_manager',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `national_id` (`national_id`),
+  UNIQUE KEY `user_name` (`user_name`),
+  UNIQUE KEY `email` (`email`),
+  UNIQUE KEY `phone` (`phone`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`id`, `user_name`, `email`, `password`, `national_id`, `birthday`, `gender`, `address`, `phone`, `thumb`, `auth`) VALUES
-(1, 'belal', 'belalmagdy261@gmail.com', '7b52009b64fd0a2a49e6d8a939753077792b0554', 321315646, '2018-02-05', 'male', 'asddsaad', '01154784216', '', 1),
-(2, 'peter', 'petershreif@gmail.com', '40bd001563085fc35165329ea1ff5c5ecbdbbeef', 32135, '0000-00-00', 'male', '', '', '', 1);
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `accounts`
---
-ALTER TABLE `accounts`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `user_acc` (`user_id`),
-  ADD KEY `bank_acc` (`bank_id`);
-
---
--- Indexes for table `banks`
---
-ALTER TABLE `banks`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `user_id` (`user_id`);
-
---
--- Indexes for table `bvn`
---
-ALTER TABLE `bvn`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `acc_id` (`acc_id`),
-  ADD KEY `bank_bvn` (`bank_id`),
-  ADD KEY `user_bvn` (`user_id`);
-
---
--- Indexes for table `transaction`
---
-ALTER TABLE `transaction`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `acc_transaction` (`acc_id`);
-
---
--- Indexes for table `transfer`
---
-ALTER TABLE `transfer`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `national_id` (`national_id`),
-  ADD UNIQUE KEY `user_name` (`user_name`),
-  ADD UNIQUE KEY `email` (`email`),
-  ADD UNIQUE KEY `phone` (`phone`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `accounts`
---
-ALTER TABLE `accounts`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `banks`
---
-ALTER TABLE `banks`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `bvn`
---
-ALTER TABLE `bvn`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `transaction`
---
-ALTER TABLE `transaction`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `transfer`
---
-ALTER TABLE `transfer`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- Constraints for dumped tables
 --
@@ -231,6 +165,14 @@ ALTER TABLE `bvn`
 --
 ALTER TABLE `transaction`
   ADD CONSTRAINT `acc_transaction` FOREIGN KEY (`acc_id`) REFERENCES `accounts` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `transfer`
+--
+ALTER TABLE `transfer`
+  ADD CONSTRAINT `acc_transaction2` FOREIGN KEY (`reciever_id`) REFERENCES `accounts` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `acc_transfer` FOREIGN KEY (`sender_id`) REFERENCES `accounts` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
