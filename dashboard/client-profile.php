a4ef30660d6c6451453ba3f8af68feda1f4468ea
Peter Sherif <petershreif@gmail.com>
2018-03-09 19:55:38 +0200

    Create Profile Page

---
+++ dashboard/client-profile.php
@@ -0,0 +1,47 @@
+<!-- Client Profile -->
+<section class="profile-section">
+	<div class="container-fluid">
+
+		<div class="row">
+			<div class="col-xs-12 col-md-3 col-md-offset-2">
+				<div class="light-box light-box--small profile-box">
+					<div class="row">
+						<div class="col-xs-12">
+							<div class="profile__avatar">
+								<img src="assets/img/avatar-placeholder.png" alt="" />
+							</div>
+							<div class="profile__basic-info">
+								<h6 class="basic-info__text">Peter Sherif Poutros</h6>
+								<p></p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div> 
+
+			<div class="col-xs-12 col-md-5">
+				<div class="light-box light-box--small profile-box">
+					<div class="row">
+						<div class="col-xs-12">
+							<div class="profile__info">
+								<p class="info__text"><span class="info__title">Acc No. :</span> 1234432223987574</p>
+								<p class="info__text"><span class="info__title">Nat ID :</span> 1234432223987574</p>
+								<p class="info__text"><span class="info__title">Email :</span> someone@example.com</p>
+								<p class="info__text"><span class="info__title">Balance :</span> 10,223,112</p>
+							</div>
+						</div>
+					</div>
+
+					<div class="row">
+						<div class="col-xs-12">
+							<div class="fr">
+								<button class="btn btn-primary--custom">Edit</button>
+								<button class="btn btn-danger--custom">Delete</button>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
