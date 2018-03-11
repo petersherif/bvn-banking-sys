9290513d1a5dca19aaf571f93644903f050e8617
Peter Sherif <petershreif@gmail.com>
2018-03-11 02:23:21 +0200

    Add Search Feature with Functionality to Search Clients & Emps

--- dashboard/search-clients.php
+++ dashboard/search-clients.php
@@ -1,6 +1,6 @@
 <?php include "./controller/SearchClientsController.php" ?>
 <!-- All Clients table -->
-<section class="dashboard-components">
+<section class="search-table">
 	<div class="container-fluid">
 
 		<div class="row">
@@ -11,30 +11,41 @@
 
 		<div class="row">
 			<div class="col-xs-12 col-md-10 col-md-offset-1">
-				<div class="light-box table-box all-clients-box">
-					<ul class="table__rows">
-						<li class="table__row">
-							<span class="row__cell row__cell--heading">Name</span>
-							<span class="row__cell row__cell--heading">Account Number</span>
-							<span class="row__cell row__cell--heading">National ID</span>
-							<span class="row__cell row__cell--heading">Balance</span>
-							<span class="row__cell row__cell--heading">Actions</span>
-						</li>
-						<?php $i=0; foreach($row as $record) 
-						{ ?>
-						<li class="table__row">
-							<span class="row__cell"><?php echo $row[$i]["user_name"] ?></span>
-							<span class="row__cell"><?php echo $row[$i]["acc_num"] ?></span>
-							<span class="row__cell"><?php echo $row[$i]["national_id"] ?></span>
-							<span class="row__cell"><?php echo $row[$i]["balance"] ?></span>
-							<span class="row__cell">
-								<a href="#" class="btn btn-primary--custom btn-sm mh2 mv1">View</a>
-								<a href="#" class="btn btn-danger--custom btn-sm mh2 mv1">Delete</a>
-							</span>
-						</li>
-						<?php $i++;
-						} ?>
-					</ul>
+				<div class="light-box table-box all-members-box">
+
+					<div class="row">
+						<div class="col-xs-12 ph4 search-bar">
+							<div class="form-group mv2">
+								<input type="search" class="form-control search-bar__input" placeholder="Search clients by name, account number, or national ID" />
+							</div>
+						</div>
+					</div>
+					
+					<div class="row">
+						<div class="col-xs-12">
+							<ul class="table__rows">
+								<li class="table__row">
+									<span class="row__cell row__cell--heading">Name</span>
+									<span class="row__cell row__cell--heading">Account Number</span>
+									<span class="row__cell row__cell--heading">National ID</span>
+									<span class="row__cell row__cell--heading">Balance</span>
+									<span class="row__cell row__cell--heading">Actions</span>
+								</li>
+								<?php $i=0; foreach($row as $record) { ?>
+								<li class="table__row data-row" data-search="<?php echo $row[$i]["user_name"] ?><?php echo $row[$i]["acc_num"] ?><?php echo $row[$i]["national_id"] ?><?php echo $row[$i]["balance"] ?>">
+									<span class="row__cell" title="<?php echo $row[$i]["user_name"] ?>"><?php echo $row[$i]["user_name"] ?></span>
+									<span class="row__cell" title="<?php echo $row[$i]["acc_num"] ?>"><?php echo $row[$i]["acc_num"] ?></span>
+									<span class="row__cell" title="<?php echo $row[$i]["national_id"] ?>"><?php echo $row[$i]["national_id"] ?></span>
+									<span class="row__cell" title="<?php echo $row[$i]["balance"] ?>"><?php echo $row[$i]["balance"] ?></span>
+									<span class="row__cell">
+										<a href="#" class="btn btn-primary--custom btn-sm mh2 mv1">View</a>
+										<a href="#" class="btn btn-danger--custom btn-sm mh2 mv1">Delete</a>
+									</span>
+								</li>
+								<?php $i++; } ?>
+							</ul>
+						</div>
+					</div>
 				</div>
 			</div> 
 		</div>
--- dashboard/search-employee.php
+++ dashboard/search-employee.php
@@ -1,6 +1,6 @@
 <?php include "./controller/SearchEmployeeController.php" ?>
-<!-- All Clients table -->
-<section class="dashboard-components">
+<!-- All Employees table -->
+<section class="search-table">
 	<div class="container-fluid">
 
 		<div class="row">
@@ -11,30 +11,43 @@
 
 		<div class="row">
 			<div class="col-xs-12 col-md-10 col-md-offset-1">
-				<div class="light-box table-box all-clients-box">
-					<ul class="table__rows">
-						<li class="table__row">
-							<span class="row__cell row__cell--heading">Name</span>
-                            <span class="row__cell row__cell--heading">National ID</span>
-							<span class="row__cell row__cell--heading">Email</span>
-							<span class="row__cell row__cell--heading">Phone</span>
-							<span class="row__cell row__cell--heading">Position</span>
-                            <span class="row__cell row__cell--heading">Actions</span>
-						</li>
-                        <?php $i=0; foreach($row as $record) { ?>
-						<li class="table__row">
-							<span class="row__cell" title="Peter Sherif"><?php echo $row[$i]["user_name"] ?></span>
-							<span class="row__cell" title="2383333282800908"><?php echo $row[$i]["national_id"] ?></span>
-							<span class="row__cell" title="2931231121221"><?php echo $row[$i]["email"] ?></span>
-                            <span class="row__cell" title="11,000,000"><?php echo $row[$i]["phone"] ?></span>
-							<span class="row__cell" title="11,000,000"><?php if($row[$i]["auth"]== 2) echo 'Manager'; else echo 'Employee' ;?></span>
-							<span class="row__cell">
-								<a href="#" class="btn btn-primary--custom btn-sm mh2 mv1">View</a>
-								<a href="#" class="btn btn-danger--custom btn-sm mh2 mv1">Delete</a>
-							</span>
-						</li>
-						<?php $i++; } ?>
-					</ul>
+				<div class="light-box table-box all-members-box">
+					
+					<div class="row">
+						<div class="col-xs-12 ph4 search-bar">
+							<div class="form-group mv2">
+								<input type="search" class="form-control search-bar__input" placeholder="Search employees by name, national ID, email, phone, or position" />
+							</div>
+						</div>
+					</div>
+
+					<div class="row">
+						<div class="col-xs-12">
+							<ul class="table__rows">
+								<li class="table__row">
+									<span class="row__cell row__cell--heading">Name</span>
+							              <span class="row__cell row__cell--heading">National ID</span>
+									<span class="row__cell row__cell--heading">Email</span>
+									<span class="row__cell row__cell--heading">Phone</span>
+									<span class="row__cell row__cell--heading">Position</span>
+							                            <span class="row__cell row__cell--heading">Actions</span>
+								</li>
+							            <?php $i=0; foreach($row as $record) { ?>
+								<li class="table__row data-row" data-search="<?php echo $row[$i]["user_name"] ?><?php echo $row[$i]["national_id"] ?><?php echo $row[$i]["email"] ?><?php echo $row[$i]["phone"] ?><?php if($row[$i]["auth"]== 2) echo 'Manager'; else echo 'Employee' ;?>">
+									<span class="row__cell" title="<?php echo $row[$i]["user_name"] ?>"><?php echo $row[$i]["user_name"] ?></span>
+									<span class="row__cell" title="<?php echo $row[$i]["national_id"] ?>"><?php echo $row[$i]["national_id"] ?></span>
+									<span class="row__cell" title="<?php echo $row[$i]["email"] ?>"><?php echo $row[$i]["email"] ?></span>
+							              <span class="row__cell" title="<?php echo $row[$i]["phone"] ?>"><?php echo $row[$i]["phone"] ?></span>
+									<span class="row__cell" title="<?php if($row[$i]["auth"]== 2) echo 'Manager'; else echo 'Employee' ;?>"><?php if($row[$i]["auth"]== 2) echo 'Manager'; else echo 'Employee' ;?></span>
+									<span class="row__cell">
+										<a href="#" class="btn btn-primary--custom btn-sm mh2 mv1">View</a>
+										<a href="#" class="btn btn-danger--custom btn-sm mh2 mv1">Delete</a>
+									</span>
+								</li>
+								<?php $i++; } ?>
+							</ul>
+						</div>
+					</div>
 				</div>
 			</div> 
 		</div>
