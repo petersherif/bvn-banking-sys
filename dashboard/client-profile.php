e95d16c5f32a05b774b64c11f0e792d0611c02e4
Eslam Abdelnasser <eslamabdelnasserr@gmail.com>
2018-04-02 18:35:34 +0200

    add profile picture and filling client data dynamicly

--- dashboard/client-profile.php
+++ dashboard/client-profile.php
@@ -8,10 +8,34 @@
 					<div class="row">
 						<div class="col-xs-12">
 							<div class="profile__avatar">
-								<img src="assets/img/avatar-placeholder.png" alt="" />
+							<!--	<img src="assets/img/avatar-placeholder.png" alt="" /> -->
+
+ 								<?php
+                                    $user_id = $_SESSION['user_id'];
+                                    $quer = "SELECT * From accounts WHERE user_id='$user_id'"; 
+                                    $query = connect()->query($quer);
+                                    while ($row = $query->fetch_object()) {
+                                    	$acc_num= $row->acc_num;
+                                    	$balance= $row->balance;
+                                    }
+
+
+                                      $quer2 = "SELECT * From users WHERE id='$user_id'"; 
+                                    $query = connect()->query($quer2);
+                                    while ($row = $query->fetch_object()) {
+                                    	$name= $row->full_name;
+                                    	$nat_id= $row->national_id;
+                                    	$email= $row->email;
+                                    	$img= $row->thumb;
+
+                                    }
+                                        ?>
+                         
+								<img src="./assets/files/users/thumb/<?php echo $img; ?>"" alt="<?php echo $name ?>" />
 							</div>
-							<div class="profile__basic-info">
-								<h6 class="basic-info__text f3 b">Peter Sherif Poutros</h6>
+
+							<div class="profile__basic-info" style="text-align: center;">
+								<h6 class="basic-info__text"><?php echo $name;?></h6>
 								<p></p>
 							</div>
 						</div>
@@ -23,11 +47,12 @@
 				<div class="light-box light-box--small profile-box">
 					<div class="row">
 						<div class="col-xs-12">
-							<div class="profile__info f4 f3-ns">
-								<p class="info__text"><span class="info__title w-30 w-20-ns dib">Acc No. :</span> 1234432223987574</p>
-								<p class="info__text"><span class="info__title w-30 w-20-ns dib">Nat ID :</span> 1234432223987574</p>
-								<p class="info__text"><span class="info__title w-30 w-20-ns dib">Email :</span> someone@example.com</p>
-								<p class="info__text"><span class="info__title w-30 w-20-ns dib">Balance :</span> 10,223,112</p>
+
+							<div class="profile__info">
+								<p class="info__text"><span class="info__title">Acc No. :</span> <?php echo $acc_num ?></p>
+								<p class="info__text"><span class="info__title">Nat ID :</span> <?php echo $nat_id ?></p>
+								<p class="info__text"><span class="info__title">Email :</span> <?php echo $email ?></p>
+								<p class="info__text"><span class="info__title">Balance :</span> <?php echo $balance ?></p>
 							</div>
 						</div>
 					</div>
