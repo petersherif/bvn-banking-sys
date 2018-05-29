05386c04a9b70cbbdea1d49cd8a52247d3b1522e
ahmed5iled <aaaahmed@gmail.com>
2018-05-29 22:04:32 +0200

    create bvn number

--- controller/MainController.php
+++ controller/MainController.php
@@ -10,7 +10,7 @@ if (isset($_POST['submit'])) {
             if ($query->num_rows != 0) {
                 while ($row = $query->fetch_assoc()) {
                     $id = $row['id'];
-                    $user_id= $row['user_id'];
+                    $user_id = $row['user_id'];
                     $bvn = $row['bvn_num'];
                     $_SESSION['loggedbvn'] = "yes";
                     $_SESSION['id'] = $sender_id;
@@ -31,4 +31,37 @@ if (isset($_POST['submit'])) {
 
     }
 }
+
+
+if (isset($_POST['submit_bvn'])) {
+    global $message;
+    $bvnNumber = $_POST['bvnNumber'];
+    $user_id = $_SESSION["user_id"];
+    if ($_POST['bvnNumber'] == "") {
+        $message = 'error';
+    } else {
+
+        $sql = "select * from bvn WHERE  user_id=$user_id";
+        $query = connect()->query($sql);
+
+        if ($query->num_rows != 0) {
+            $message = "exist";
+        } else {
+            $values = "'" . $_POST['bvnNumber'] . "','" . $_SESSION["user_id"] . "'";
+            $sql3 = "insert into bvn (`bvn_num`,`user_id`)VALUES ($values)";
+            $query = connect()->query($sql3);
+            $link = mysqli_connect("localhost", "root", "", "bvn_system");
+            mysqli_query($link, $sql3);
+            $last_id = mysqli_insert_id($link);
+            $acc_id = $_SESSION['id'];
+
+            $sql = "insert into bvn_acc (`bvn_id`,`acc_id`)VALUES ($last_id,$acc_id)";
+            $query = connect()->query($sql);
+            $message = 'success';
+
+        }
+    }
+}
+
+
 ?>
--- controller/addClientController.php
+++ controller/addClientController.php
@@ -17,14 +17,7 @@ if (isset($_POST['submit'])) {
     } elseif ($_POST['address'] == "") {
         $message = 'error';
     } else {
-
-        $sql = "select * from users WHERE email='$email'";
-        $query = connect()->query($sql);
-
-        if ($query->num_rows != 0) {
-            $message = "exist";
-        } else {
-            $message = "success";
+        if ($_POST['bvn_check']) {
             $values = "'" . $_POST['full_name'] . "','" . $_POST['email'] . "','" . $_POST['national_id'] . "','" . $_POST['birthday'] . "','" . $_POST['gender'] . "','" . $_POST['address'] . "','" . $_POST['phone'] . "','0','" . $_SESSION["bank_id"] . "'";
             $sql = "INSERT INTO users (`full_name`,`email`,`national_id`,`birthday`,`gender`,`address`,`phone`,`auth`,bank_id)VALUES ($values)";
             $query = connect()->query($sql);
@@ -32,16 +25,80 @@ if (isset($_POST['submit'])) {
             $query = connect()->query($sql);
             $row = $query->fetch_assoc();
             $id = $row['id'];
-            $sql = "SELECT acc_num from accounts ORDER BY acc_num DESC LIMIT 1";
+            $sql = "SELECT bvn_num from bvn ORDER BY bvn_num DESC LIMIT 1";
             $query = connect()->query($sql);
             $row = $query->fetch_assoc();
             if (count($row) == 0)
-                $acc_num = 10000;
+                $bvnNumber = 100000000;
             else
-                $acc_num = $row['acc_num'] + 1;
-            $sql = "INSERT INTO accounts (acc_num , user_id) VALUES ($acc_num,$id)";
+                $bvnNumber = $row['bvn_num'] + 1;
+
+            $sql = "INSERT INTO bvn (bvn_num,user_id)VALUES ($bvnNumber,$id)";
+            $query = connect()->query($sql);
+            $message = "success";
+
+
+        } elseif ($_POST['acc_check']) {
+
+            $sql = "select * from users WHERE email='$email'";
             $query = connect()->query($sql);
 
+            if ($query->num_rows != 0) {
+                $message = "exist";
+            } else {
+                $message = "success";
+                $values = "'" . $_POST['full_name'] . "','" . $_POST['email'] . "','" . $_POST['national_id'] . "','" . $_POST['birthday'] . "','" . $_POST['gender'] . "','" . $_POST['address'] . "','" . $_POST['phone'] . "','0','" . $_SESSION["bank_id"] . "'";
+                $sql = "INSERT INTO users (`full_name`,`email`,`national_id`,`birthday`,`gender`,`address`,`phone`,`auth`,bank_id)VALUES ($values)";
+                $query = connect()->query($sql);
+                $sql = "SELECT id from users ORDER BY id DESC LIMIT 1";
+                $query = connect()->query($sql);
+                $row = $query->fetch_assoc();
+                $id = $row['id'];
+                $sql = "SELECT acc_num from accounts ORDER BY acc_num DESC LIMIT 1";
+                $query = connect()->query($sql);
+                $row = $query->fetch_assoc();
+                if (count($row) == 0)
+                    $acc_num = 10000;
+                else
+                    $acc_num = $row['acc_num'] + 1;
+                $sql = "INSERT INTO accounts (acc_num , user_id) VALUES ($acc_num,$id)";
+                $query = connect()->query($sql);
+
+            }
+        } else {
+            $sql = "select * from users WHERE email='$email'";
+            $query = connect()->query($sql);
+
+            if ($query->num_rows != 0) {
+                $message = "exist";
+            } else {
+                $message = "success";
+                $values = "'" . $_POST['full_name'] . "','" . $_POST['email'] . "','" . $_POST['national_id'] . "','" . $_POST['birthday'] . "','" . $_POST['gender'] . "','" . $_POST['address'] . "','" . $_POST['phone'] . "','0','" . $_SESSION["bank_id"] . "'";
+                $sql2 = "INSERT INTO users (`full_name`,`email`,`national_id`,`birthday`,`gender`,`address`,`phone`,`auth`,bank_id)VALUES ($values)";
+                $query = connect()->query($sql2);
+                $sql = "SELECT id from users ORDER BY id DESC LIMIT 1";
+                $query = connect()->query($sql);
+                $row = $query->fetch_assoc();
+                $id = $row['id'];
+                $sql = "SELECT acc_num from accounts ORDER BY acc_num DESC LIMIT 1";
+                $query = connect()->query($sql);
+                $row = $query->fetch_assoc();
+                if (count($row) == 0)
+                    $acc_num = 10000;
+                else
+                    $acc_num = $row['acc_num'] + 1;
+                $sql = "INSERT INTO accounts (acc_num , user_id) VALUES ($acc_num,$id)";
+                $query = connect()->query($sql);
+
+                $link = mysqli_connect("localhost", "root", "", "bvn_system");
+                mysqli_query($link, $sql2);
+                $last_id = mysqli_insert_id($link);
+
+                $sql = "INSERT INTO bvn (bvn_num,user_id)VALUES ($bvnNumber,$last_id)";
+                $query = connect()->query($sql);
+
+
+            }
         }
     }
 
