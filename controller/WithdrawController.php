d6b469bf50c9e610d5ded8c8b2e8ce1fd32fd860
belal <belalmagdy261@gmail.combelalmagdy261@gmail.com>
2018-06-02 13:45:52 +0200

    Handle Balance Error ( - balance )

--- controller/WithdrawController.php
+++ controller/WithdrawController.php
@@ -1,10 +1,19 @@
 <?php
-if (isset($_POST['amount'])) {
+ $account_id = $_SESSION['id'];
+$sql = "SELECT * FROM accounts WHERE id='$account_id'";
+$query = connect()->query($sql);
+if ($query->num_rows > 0) {
+    while ($row = $query->fetch_assoc()) {
+        $balance = $row['balance'];
+    }
+}
+
+if (isset($_POST['amount']) && $balance > $_POST['amount']) {
     $amount = $_POST['amount'];
     $account_id = $_SESSION['id'];
     $account = $_SESSION['bvn'];
-    $description = "You have Withdrawed: ".$amount . " L.E";
-    $sql = "INSERT INTO `transaction`(`amount`,`type`,`acc_id`,`description`)VALUE ($amount,1,$account_id,$description)";
+    $description = "You have Withdrawed: ".$amount. " L.E";
+    $sql = "INSERT INTO `transaction`(`amount`,`type`,`acc_id`,`description`)VALUE ($amount,1,$account_id,'$description')";
     $query = connect()->query($sql);
     ?><?php
     $sql = "SELECT * FROM accounts WHERE id='$account_id'";
@@ -13,9 +22,9 @@ if (isset($_POST['amount'])) {
         while ($row = $query->fetch_assoc()) {
             $balance = $row['balance'];
         }
-        $newBalance = $balance - $amount;
-        $sql = "UPDATE `accounts` SET balance='$newBalance' WHERE id='$account_id'";
-        $query = connect()->query($sql);
+            $newBalance = $balance - $amount;
+            $sql = "UPDATE `accounts` SET balance='$newBalance' WHERE id='$account_id'";
+            $query = connect()->query($sql);
     }
 
 
