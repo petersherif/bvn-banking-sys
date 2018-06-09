1a51ab5c82b88dac7aee813c7d7d23099962f206
ahmed5iled <aaaahmed@gmail.com>
2018-06-09 17:23:13 +0200

    fixed add emp

--- controller/EmployeeController.php
+++ controller/EmployeeController.php
@@ -36,8 +36,8 @@ if (isset($_POST['submit'])) {
             $imageFileType = pathinfo($target_file, PATHINFO_EXTENSION);
             move_uploaded_file($_FILES["thumb"]["tmp_name"], $target_file);
 
-            $values = "'" . $_POST['user_name'] . "','" . $_POST['full_name'] . "','" . $_POST['email'] . "','" . md5($_POST['password']) . "','" . $_POST['national_id'] . "','" . $_POST['birthday'] . "','" . $_POST['gender'] . "','" . $_POST['address'] . "','" . $_POST['phone'] . "','" . $_FILES["thumb"]["name"] . "','" . $_POST['auth'] . "','" . $_SESSION["bank_id"] . "'";
-            $sql = "insert into users (`user_name`,`full_name`,`email`,`password`,`national_id`,`birthday`,`gender`,`address`,`phone`,`thumb`,`auth`,bank_id)VALUES ($values)";
+            $values = "'" . $_POST['user_name'] . "','" . $_POST['full_name'] . "','" . $_POST['email'] . "','" . md5($_POST['password']) . "','" . $_POST['national_id'] . "','" . $_POST['birthday'] . "','" . $_POST['gender'] . "','" . $_POST['address'] . "','" . $_POST['phone'] . "','" . $_FILES["thumb"]["name"] . "','" . $_POST['auth'] . "'";
+            $sql = "insert into users (`user_name`,`full_name`,`email`,`password`,`national_id`,`birthday`,`gender`,`address`,`phone`,`thumb`,`auth`)VALUES ($values)";
             $query = connect()->query($sql);
             $message = 'success';
 
